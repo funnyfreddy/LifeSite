224c359307655d05bc7911ed1deb6554bc9b91a1
Kristen Malone <kristen.marie.malone@gmail.com>
2015-05-31 19:11:32 -0400

    added some javascript, worked on love page, added css

--- java/app.js
+++ java/app.js
@@ -1,5 +1,28 @@
 var main = function() {
 
+$('.questionLeadin').click(function(){
+	// remove the Qcurrent class from other active classes to hide these and only display the question you're working on
+	// $('.questionLeadin').removeClass('Qcurrent');
+	// $('questions').removeClass('Qscurrent');
+	
+	// add current classes to the element clicked on - the question lead in - and the next element, the question & answer bundling in ".questions" div class - which will show them
+	$(this).addClass('Qcurrent');
+	$(this).next('.questions').addClass('Qscurrent');
+
+	// from there - if you click on the right answer, alert that they got it right and move on to next question - if not, confirm as to whether the person wants to move on bc they go it wrong
+	$('.answers').click(function() {
+		if($(this).hasClass('right')) {
+			// alert("great- you're totally crushing it");
+			var L1 = $(this).parentsUntil('.questions');
+			L1.next('.questionsLeadin').addClass('Qcurrent');
+		} else {
+			confirm("umm.. not going so well so far - you sure you want to continue with this?");
+			$(this).parentsUntil('.questions').next('.questionsLeadin').addClass('Qcurrent');
+		}
+	});
+});
+
+
 
 
 }

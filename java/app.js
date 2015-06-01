fc6a74c7bfa8d29b86797616e2f93315ea3cd02f
Kristen Malone <kristen.marie.malone@gmail.com>
2015-05-31 22:14:06 -0400

    yay got next question to pop up finally

--- java/app.js
+++ java/app.js
@@ -8,20 +8,19 @@ $('.questionLeadin').click(function(){
 	// add current classes to the element clicked on - the question lead in - and the next element, the question & answer bundling in ".questions" div class - which will show them
 	$(this).addClass('Qcurrent');
 	$(this).next('.questions').addClass('Qscurrent');
+});
 
 	// from there - if you click on the right answer, alert that they got it right and move on to next question - if not, confirm as to whether the person wants to move on bc they go it wrong
 	$('.answers').click(function() {
+		var answerSelected = $(this);
 		if($(this).hasClass('right')) {
-			// alert("great- you're totally crushing it");
-			var L1 = $(this).parentsUntil('.questions');
-			L1.next('.questionsLeadin').addClass('Qcurrent');
+			alert("great- you're totally crushing it");
+			answerSelected.parents('.questions').next('.questionLeadin').addClass('Qcurrent');
 		} else {
 			confirm("umm.. not going so well so far - you sure you want to continue with this?");
-			$(this).parentsUntil('.questions').next('.questionsLeadin').addClass('Qcurrent');
+			answerSelected.parents('.questions').next('.questionLeadin').addClass('Qcurrent');
 		}
 	});
-});
-
 
 
 
